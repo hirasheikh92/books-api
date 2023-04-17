e7cedb808da24dcd24137de95e32e9eb4f5955bc
hirasheikh92 <hiramoueen@gmail.com>
2023-04-17 17:26:08 +0500

    update

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://simple-books-api.glitch.me/:path*",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
